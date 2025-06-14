ae01bec83f34d2c3254d4ca685b4e6c53229355f
Hexagon <seedindeserd@gmail.com>
2025-06-15 01:20:32 +0300

    Fixed base.

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@
 
 
 int METHOD;
-std::string IMAGE_PATH;
 std::string SENS_DATA;
+std::string IMAGE_PATH;
+std::string OUTPUT_PATH;
 
 
 std::string read_file(const std::string &filename){
@@ -19,7 +20,7 @@ std::string read_file(const std::string &filename){
 			res += buff + '\n';
 		}
 	} else{
-		std::cout << "Error: CAN NOT OPEN FILE. Name: " << filename;
+		std::cerr << "Error: CAN NOT OPEN FILE. Name: " << filename;
 	}
 
 	f.close();
@@ -31,21 +32,22 @@ std::string read_file(const std::string &filename){
 
 int main(int argc, char const *argv[]){
 	// Hello banner
-	std::cout << "This is AIP project that can help to hide your data with stegonography algorithms.\n\nUsage: main.exe METHOD \"IMAGE_TO_HIDE.png\" \"Some sensetive data to hide\".\n\nOr just use flag -F to work with file where placed sensetive data.\n";
+	std::cout << "This is AIP project that can help to hide your data with stegonography algorithms.\n\nUsage: main.exe METHOD \"IMAGE_TO_HIDE.png\" \"OUTPUT_FILE.png\" \"Some sensetive data to hide\".\n\nOr just use flag -F to work with file where placed sensetive data.\n";
 
 	if (argc <= 1){
-		std::cout << "Error: Incorrect argumets." << std::endl;
+		std::cerr << "Error: Incorrect argumets." << std::endl;
 		return 1;
 	}
 
 	// Putting arguments
 	METHOD = atoi(argv[1]);
 	IMAGE_PATH = argv[2];
+	OUTPUT_PATH = argv[3];
 
-	if (std::string(argv[3]) == "-F"){
-		SENS_DATA = read_file(argv[4]);
+	if (std::string(argv[4]) == "-F"){
+		SENS_DATA = read_file(argv[5]);
 	} else{
-		SENS_DATA = argv[3];
+		SENS_DATA = argv[4];
 	}
 
 	return 0;
